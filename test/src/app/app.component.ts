fa1b05df8209a4759199c423548d272b72f8f596
maxlongint <fillchar@hotmail.com>
2023-08-03 18:04:30 +0800

    更新到12.0.11

--- test/src/app/app.component.ts
+++ test/src/app/app.component.ts
@@ -11,8 +11,6 @@ import { environment } from '../environments/environment';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit, AfterViewInit, AfterContentInit {
-    @Store()
-    visible = false;
     constructor() {}
 
     ngOnInit(): void {}
