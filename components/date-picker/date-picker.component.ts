8ab42ebf653057e26c007b06deed1a35b90bb8e2
maxlongint <fillchar@hotmail.com>
2023-08-08 09:38:15 +0800

    时间录入框增加选择日期面板

--- components/date-picker/date-picker.component.ts
+++ components/date-picker/date-picker.component.ts
@@ -1,6 +1,21 @@
-import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { CdkOverlayOrigin, Overlay, OverlayConfig, OverlayRef } from '@angular/cdk/overlay';
+import { TemplatePortal } from '@angular/cdk/portal';
+import { DOCUMENT } from '@angular/common';
+import {
+    Component,
+    ElementRef,
+    Inject,
+    Input,
+    OnInit,
+    Renderer2,
+    TemplateRef,
+    ViewChild,
+    ViewContainerRef,
+    forwardRef,
+} from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { NzDestroyService } from 'ng-zorro-antd/core/services';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
     selector: 'ngx-date-picker',
@@ -37,7 +52,13 @@ export class NgxDatePickerComponent implements OnInit, ControlValueAccessor {
      */
     @Input() mode: 'date' | 'month' | 'year' = 'date';
 
-    constructor() {}
+    constructor(
+        private destroy: NzDestroyService,
+        private overlay: Overlay,
+        private viewContainerRef: ViewContainerRef,
+        private renderer: Renderer2,
+        @Inject(DOCUMENT) private document: Document
+    ) {}
 
     ngOnInit(): void {}
 
@@ -46,7 +67,9 @@ export class NgxDatePickerComponent implements OnInit, ControlValueAccessor {
     onChange: (value: string | null) => void = () => null;
     onTouched: () => void = () => null;
 
-    writeValue(obj: any): void {}
+    writeValue(obj: any): void {
+        this.value = obj;
+    }
     registerOnChange(fn: any): void {
         this.onChange = fn;
     }
@@ -56,5 +79,129 @@ export class NgxDatePickerComponent implements OnInit, ControlValueAccessor {
     setDisabledState?(isDisabled: boolean): void {
         this.disabled = isDisabled;
         this.placeholder = '';
+    }
+
+    @ViewChild('originElement') originElement!: CdkOverlayOrigin;
+    @ViewChild('inputElement') inputElement!: ElementRef;
+    @ViewChild('dateTimePickerTemplate') dateTimePickerTemplate!: TemplateRef<ElementRef>;
+
+    /**
+     * 浮层对象
+     */
+    overlayRef?: OverlayRef;
+    /**
+     * 渲染模板
+     */
+    templatePortal?: TemplatePortal;
+    /**
+     * overlay浮层是否显示
+     */
+    visible = false;
+
+    open() {
+        this.attachOverlay();
+        this.visible = true;
+        this.updateOverlayStyle();
+        this.updateBodyOverflow();
+    }
+
+    close() {
+        this.visible = false;
+        this.updateOverlayStyle();
+        this.updateBodyOverflow();
+        this.disposeOverlay();
+    }
+
+    private attachOverlay(): void {
+        this.templatePortal = new TemplatePortal(this.dateTimePickerTemplate, this.viewContainerRef);
+        this.overlayRef = this.overlay.create(this.getOverlayConfig());
+
+        if (this.overlayRef && !this.overlayRef.hasAttached()) {
+            this.overlayRef.attachments().subscribe(() => {});
+            this.overlayRef.attach(this.templatePortal);
+            this.overlayRef
+                .outsidePointerEvents()
+                .pipe(takeUntil(this.destroy))
+                .subscribe((event: MouseEvent) => {
+                    // 点击的是input就不触发
+                    if (event.target === this.inputElement.nativeElement) {
+                        event.stopPropagation();
+                        return;
+                    }
+
+                    this.close();
+                });
+        }
+    }
+
+    private getOverlayConfig(): OverlayConfig {
+        const { nativeElement } = this.originElement.elementRef;
+        const positionStrategy = this.overlay
+            .position()
+            .flexibleConnectedTo(nativeElement)
+            .withPositions([
+                {
+                    originX: 'start',
+                    originY: 'bottom',
+                    overlayX: 'start',
+                    overlayY: 'top',
+                    offsetX: 0,
+                    offsetY: 0,
+                },
+                {
+                    originX: 'start',
+                    originY: 'top',
+                    overlayX: 'start',
+                    overlayY: 'bottom',
+                    offsetX: 0,
+                    offsetY: 0,
+                },
+                {
+                    originX: 'end',
+                    originY: 'bottom',
+                    overlayX: 'end',
+                    overlayY: 'top',
+                    offsetX: 0,
+                    offsetY: 0,
+                },
+                {
+                    originX: 'end',
+                    originY: 'top',
+                    overlayX: 'end',
+                    overlayY: 'bottom',
+                    offsetX: 0,
+                    offsetY: 0,
+                },
+            ])
+            .withPush(false);
+        const scrollStrategy = this.overlay.scrollStrategies.close();
+        return new OverlayConfig({
+            positionStrategy,
+            // 这里滚动策略是不生效的，因为并不是body的滚动
+            scrollStrategy,
+            hasBackdrop: false,
+            disposeOnNavigation: true,
+        });
+    }
+
+    private disposeOverlay(): void {
+        this.overlayRef?.dispose();
+        this.overlayRef = undefined;
+    }
+
+    private updateOverlayStyle(): void {
+        if (this.overlayRef && this.overlayRef.overlayElement) {
+            this.renderer.setStyle(this.overlayRef.overlayElement, 'pointer-events', this.visible ? 'auto' : 'none');
+        }
+    }
+
+    private updateBodyOverflow(): void {
+        if (this.overlayRef) {
+            if (this.visible) {
+                this.overlayRef.getConfig().scrollStrategy!.enable();
+            } else {
+                this.overlayRef.getConfig().scrollStrategy!.disable();
+            }
+        }
     }
 }
--- components/date-picker/date-picker.module.ts
+++ components/date-picker/date-picker.module.ts
@@ -4,10 +4,12 @@ import { NgxDatePickerComponent } from './date-picker.component';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { FormsModule } from '@angular/forms';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
 
 @NgModule({
     declarations: [NgxDatePickerComponent],
-    imports: [CommonModule, NzInputModule, NzIconModule, FormsModule],
+    imports: [CommonModule, FormsModule, OverlayModule, NzInputModule, NzIconModule, NzDatePickerModule],
     exports: [NgxDatePickerComponent],
 })
 export class NgxDatePickerModule {}
