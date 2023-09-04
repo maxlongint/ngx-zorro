f4f54daf81cb0a68206e0df9ba1ba66f6b1dd307
maxlongint <fillchar@hotmail.com>
2023-09-04 16:56:54 +0800

    增加loading组件从服务调用

---
+++ components/loading/loading-container.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+    selector: 'ngx-loading-container',
+    template: `
+        <ngx-loading [(visible)]="visible" [tip]="tip"></ngx-loading>
+    `,
+})
+export class NgxLoadingContainerComponent implements OnInit {
+    /**
+     * 是否显示
+     */
+    @Input() visible = false;
+    /**
+     * 提示内容
+     */
+    @Input() tip = '加载中…';
+
+    constructor() {}
+
+    ngOnInit(): void {}
+
+    open() {
+        this.visible = true;
+    }
+
+    close(): void {}
+}
--- components/loading/loading.module.ts
+++ components/loading/loading.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { NgxLoadingComponent } from './loading.component';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { PortalModule } from '@angular/cdk/portal';
+import { NgxLoadingContainerComponent } from './loading-container.component';
 
 @NgModule({
-    declarations: [NgxLoadingComponent],
+    declarations: [NgxLoadingComponent, NgxLoadingContainerComponent],
     imports: [CommonModule, OverlayModule, PortalModule],
-    exports: [NgxLoadingComponent],
+    exports: [NgxLoadingComponent, NgxLoadingContainerComponent],
 })
 export class NgxLoadingModule {}
