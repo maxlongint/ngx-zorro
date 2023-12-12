2897a77e3277238e99a92dfa727e387d806cce5b
maxlongint <fillchar@hotmail.com>
2023-12-12 09:46:30 +0800

    删除console

--- components/services/cache.service.ts
+++ components/services/cache.service.ts
@@ -9,7 +9,6 @@ export class NgxCacheService {
     private cache: Map<string, Observable<HttpEvent<any>>> = new Map<string, Observable<HttpEvent<any>>>();
 
     get<T>(key: string): Observable<HttpEvent<T>> | undefined {
-        console.dir(this.cache);
         return this.cache.get(key) as Observable<HttpEvent<T>>;
     }
 
