ae648ba6e057cc53325e7281739f894882ffb9d6
maxlongint <fillchar@hotmail.com>
2024-01-02 15:47:26 +0800

    缓存请求改为只支持get

--- components/interceptors/cache.interceptor.ts
+++ components/interceptors/cache.interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 import { NgxCacheService } from 'ngx-zorro/services';
@@ -12,36 +12,15 @@ export class NgxCacheInterceptor implements HttpInterceptor {
         // 1.判断是GET
         // 2.header中添加 Cache-Map: Storage
         const cacheMap = request.headers.get('Cache-Map');
-        if (cacheMap !== 'Storage') {
+        if (cacheMap !== 'Storage' || request.method !== 'GET') {
             return next.handle(request);
         }
-        let key: string | null = this.getCodePointsFromString(request.url);
-        if (request.method !== 'GET') {
-            key = this.getCodePointsFromString(request.headers.get('Cache-Map-Key') ?? '');
-            if (!key) {
-                throw new Error('缓存post请求需要在header中传递Cache-Map-Key');
-            }
-        }
-        const cachedResponse = this.cache.get(key);
+        const cachedResponse = this.cache.get(request.url);
         if (cachedResponse) {
             return cachedResponse as Observable<HttpEvent<T>>;
         }
-        request = request.clone({ headers: request.headers.delete('Cache-Map-Key') });
         const response = next.handle(request).pipe(shareReplay(1));
-        this.cache.set(key, response);
+        this.cache.set(request.url, response);
         return response;
     }
-
-    private getCodePointsFromString(key: string) {
-        const codePoints = [];
-        for (let i = 0; i < key.length; i++) {
-            codePoints.push(key.codePointAt(i));
-        }
-        return codePoints.join('');
-    }
 }
-
-/**
- * 缓存请求拦截器注入内容
- */
-export const NgxCacheInterceptorProvide = [{ provide: HTTP_INTERCEPTORS, useClass: NgxCacheInterceptor, multi: true }];
